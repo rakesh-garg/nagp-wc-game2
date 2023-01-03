f761091310e51eee993197cbf295c31f4cb98ad0
Rakesh Garg <Rakesh.Garg@blackbaud.me>
2023-01-03 10:30:22 -0500

    updated Details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,9 @@ import { BoardComponent } from './board-game/board.component';
 })
 export class AppComponent {
   title = 'BoardGame';
-  constructor(private injector: Injector) {
-    const el = createCustomElement(BoardComponent, { injector });
-    customElements.define('board-game', el);
-  }
-  ngDoBootstrap() {}
+  // constructor(private injector: Injector) {
+  //   const el = createCustomElement(BoardComponent, { injector });
+  //   customElements.define('nagp-board-game', el);
+  // }
+  // ngDoBootstrap() {}
 }
